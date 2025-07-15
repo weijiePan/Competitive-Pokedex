cdeba5e403457689a31817608a404f5d7eb74595
wp <97200483+weijiePan@users.noreply.github.com>
2025-07-15 12:43:32 -0400

    navBar styling

--- src/components/general/PageNav.jsx
+++ src/components/general/PageNav.jsx
@@ -1,10 +1,10 @@
 import List from "../pages/List";
-
+import "@css/general/pageNav.css";
 function PageNav({ setPage}){
-    const pages  = [{name:"list", comp:<List/>}];
+    const pages  = [{name:"List", comp:<List/>}];
     return(
         <nav className="pageNav">
-            {pages.map((item)=> <div key={item.name} onClick={()=>{setPage(pages[0].comp)}}><h3>{item.name}</h3></div>)}
+            {pages.map((item)=> <div key={item.name} className = "navHeading" onClick={()=>{setPage(pages[0].comp)}}><h3>{item.name}</h3></div>)}
         </nav>
     )
 }
