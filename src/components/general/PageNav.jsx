277ea1616ee5cef9ec56ebc7685be968e6fe10f5
wp <97200483+weijiePan@users.noreply.github.com>
2025-07-17 22:16:53 -0400

    css update

--- src/components/general/PageNav.jsx
+++ src/components/general/PageNav.jsx
@@ -1,9 +1,11 @@
 import List from "../pages/List";
 import "@css/general/pageNav.css";
+import pokdex from "@assets/pokedex-color.svg"
 function PageNav({ setPage}){
     const pages  = [{name:"List", comp:<List/>}];
     return(
         <nav className="pageNav">
+            <img src = {pokdex} className="logo"></img>
             {pages.map((item)=> <div key={item.name} className = "navHeading" onClick={()=>{setPage(pages[0].comp)}}><h3>{item.name}</h3></div>)}
         </nav>
     )
