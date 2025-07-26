4c5dfe79753e2547e3ae2a2ab1ddd215152c6229
weijiePan <97200483+weijiePan@users.noreply.github.com>
2025-07-26 04:49:19 -0400

    change

--- src/components/listView/PkmonDisplay.jsx
+++ src/components/listView/PkmonDisplay.jsx
@@ -1,16 +1,8 @@
-function PkmonDisplay({pkmon, setPkmonObj}){
-    
+
+function PkmonDisplay(pkmonObj){
     return(
-        <div className="pkmonDisplay" >
-            <div className = "pkmonNameHeading">
-                <h3 className = "pkmonName">{pkmon.name}</h3>
-                <h3>{pkmon.id}</h3>
-            </div>
-            <img src = {pkmon.sprites.front_default}></img>
-            <button onClick={()=>{
-                setPkmonObj(pkmon)
-            }}>Learn more</button>
-        </div>
+        {
+            
+        }
     )
 }
-export default PkmonDisplay;
